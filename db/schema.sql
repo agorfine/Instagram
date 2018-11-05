eafd5068a51670ab3fec4080b7b29d79cb84bd1c
Cynthia Nguyen <cynthia@ool-ad0201be.dyn.optonline.net>
2018-11-05 15:56:30 -0500

    updated schema and seed

--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,27 @@ CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
   password VARCHAR(50) NOT NULL,
-  full_name TEXT(50),
+  full_name TEXT,
   phone VARCHAR(50),
   bio VARCHAR(255),
-  profpic BLOB,
+  profpic_url VARCHAR(255)
 );
 
 CREATE TABLE If NOT EXISTS pictures (
   id SERIAL PRIMARY KEY,
   user_id INT NOT NULL,
-  date_time DATETIME NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
+  img_url VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE If NOT EXISTS comments (
   id SERIAL PRIMARY KEY,
   picture_id INT NOT NULL,
   user_id INT NOT NULL,
-  comment VARCHAR(255) NOT NULL,
+  comment VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS likes (
   id SERIAL PRIMARY KEY,
   picture_id INT NOT NULL,
-  user_id INT NOT NULL,
+  user_id INT NOT NULL
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,19 +1,19 @@
-\c buildingdb
+\c finstadb
 
 INSERT INTO users VALUES (1, 'rossi', 'password', 'Valentino Rossi', null, null, null);
 INSERT INTO users VALUES (2, 'lilC', 'password', 'Cynthia ', null, null, null);
 INSERT INTO users VALUES (3, 'uhmandur', 'password', 'Amanda', null, null, null);
 
-INSERT INTO pictures VALUES (DEFAULT, 1, '', 'https://res.cloudinary.com/dvjtpejbw/video/upload/v1541448984/EVPoaa6.mp4');
-INSERT INTO pictures VALUES (DEFAULT, 1, '', 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449327/Screenshot_20181105-152129_Instagram.jpg');
-INSERT INTO pictures VALUES (DEFAULT, 1, '', 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449314/Screenshot_20181105-152100_Instagram.jpg');
-INSERT INTO pictures VALUES (DEFAULT, 2, '', 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1537200689/crispy-chocolate-chip-cookies.jpg');
-INSERT INTO pictures VALUES (DEFAULT, 2, '', 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449549/Screenshot_20181105-152500_Instagram.jpg');
-INSERT INTO pictures VALUES (DEFAULT, 2, '', 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449655/Snapchat-450742301.jpg');
-INSERT INTO pictures VALUES (DEFAULT, 2, '', 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449794/Screenshot_20181105-152932_Instagram.jpg');
-INSERT INTO pictures VALUES (1, 3, '', 'https://secure.cdn1.wdpromedia.com/resize/mwImage/1/630/354/75/dam/wdpro-assets/parks-and-tickets/entertainment/magic-kingdom/move-it-shake-it-dance-play-it-street-party/move-it-shake-it-dance-play-it-00.jpg?1520709311202');
-INSERT INTO pictures VALUES (DEFAULT, 3, '', 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449233/Screenshot_20181105-151934_Instagram.jpg');
-INSERT INTO pictures VALUES (DEFAULT, 3, '', 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449879/Screenshot_20181105-153052_Instagram.jpg');
+INSERT INTO pictures VALUES (DEFAULT, 1, 'https://res.cloudinary.com/dvjtpejbw/video/upload/v1541448984/EVPoaa6.mp4');
+INSERT INTO pictures VALUES (DEFAULT, 1, 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449327/Screenshot_20181105-152129_Instagram.jpg');
+INSERT INTO pictures VALUES (DEFAULT, 1, 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449314/Screenshot_20181105-152100_Instagram.jpg');
+INSERT INTO pictures VALUES (DEFAULT, 2, 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1537200689/crispy-chocolate-chip-cookies.jpg');
+INSERT INTO pictures VALUES (DEFAULT, 2, 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449549/Screenshot_20181105-152500_Instagram.jpg');
+INSERT INTO pictures VALUES (DEFAULT, 2, 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449655/Snapchat-450742301.jpg');
+INSERT INTO pictures VALUES (DEFAULT, 2, 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449794/Screenshot_20181105-152932_Instagram.jpg');
+INSERT INTO pictures VALUES (default, 3, 'https://secure.cdn1.wdpromedia.com/resize/mwImage/1/630/354/75/dam/wdpro-assets/parks-and-tickets/entertainment/magic-kingdom/move-it-shake-it-dance-play-it-street-party/move-it-shake-it-dance-play-it-00.jpg?1520709311202');
+INSERT INTO pictures VALUES (DEFAULT, 3, 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449233/Screenshot_20181105-151934_Instagram.jpg');
+INSERT INTO pictures VALUES (DEFAULT, 3, 'https://res.cloudinary.com/dvjtpejbw/image/upload/v1541449879/Screenshot_20181105-153052_Instagram.jpg');
 
 INSERT INTO comments VALUES (DEFAULT, 1, 1, 'SICKKKKK');
 INSERT INTO comments VALUES (DEFAULT, 1, 2, 'youre the cutest');
