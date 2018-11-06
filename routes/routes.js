5adfb88b02911d3bdbc1242750f2b4139d9f5937
Cynthia Nguyen <cynthia@ool-ad0201be.dyn.optonline.net>
2018-11-06 14:26:31 -0500

    new route controller model for create new acct page

--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,8 @@ const controller = require('../controller/controller')
 
 routes.get('/newsfeed', controller.index);    //route to explore page
 routes.post('/userauth', controller.user);    //route to check login au
-routes.get('/:id', controller.show);
-routes.post('/', controller.create);
+routes.get('/:id', controller.show);          //route to profile page
+routes.post('/', controller.create);          //route to create new account page
 routes.put('/:id', controller.update);
 routes.delete('/:id', controller.destroy);
 
