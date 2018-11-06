944d980f9fc2dada38c1ea3a5160f3d0450afea4
Amanda Gorfine <amandagorfine@ool-ad0203d0.dyn.optonline.net>
2018-11-06 14:05:49 -0500

    newsfeed updated to include obj newsfeed instead of table pictures much better! PROXY CHANGED to include /finsta

--- client/src/Components/Picture.js
+++ client/src/Components/Picture.js
@@ -1,38 +1,16 @@
 import React, { Component } from 'react';
-import UsernameCaption from './UsernameCaption'
 import axios from 'axios';
 
 class Picture extends Component {
-	state = {
-	    apiDataLoaded: false,
-	    apiData: null,
-	  }
-
-	componentDidMount() {
-    	axios.get('/finsta/users')
-     	.then( res => {
-        this.setState(prevState => ({
-          apiDataLoaded: true,
-          apiData: res.data.data
-        }))
-      })
-	}
-
-	renderUserName() {
-		if(this.state.apiDataLoaded) {
-			return this.state.apiData.map(d => {
-				return(
-					<UsernameCaption key ={d.id} username={d} />
-				)
-			})
-		} else return <p>Loading...</p>
-	}
 
 	render () {
 		return (
 			<div>
 				<img className="picture" src={this.props.picture.img_url} alt="Camera"/>
-				
+				<div>
+					<div className="username">{this.props.picture.username}</div>
+					<div>{this.props.picture.caption}</div>
+				</div>
 			</div>
 		)
 	}
