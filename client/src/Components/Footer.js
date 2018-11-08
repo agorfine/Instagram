a6f1e9cff4ff0dd7c25342606ef32e08ea919f1e
Amanda Gorfine <amandagorfine@ool-ad0203d0.dyn.optonline.net>
2018-11-08 09:28:22 -0500

    footer onclick fixed by rachel

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -7,8 +7,6 @@ class Footer extends Component {
 	state = {
 		isHome: false,
 		isProf: true,
-		isHomeNow: false,
-		isProfNow: true
 	}
 
 	handleClick(e) {
@@ -16,20 +14,20 @@ class Footer extends Component {
 		console.log('inside handleClick')
 
 		this.setState(prevState => ({
-			isHome: !prevState.isLike,
-			isProf: !prevState.isProf
+			isHome: !this.state.isHome,
+			isProf: !this.state.isProf
 		}))
 	}
 
-	homeClick(e) {
-		e.stopPropagation()
-		console.log('inside handleClick')
+	// homeClick(e) {
+	// 	e.stopPropagation()
+	// 	console.log('inside homeClick')
 
-		this.setState(prevState => ({
-			isHomeNow: !prevState.isLike,
-			isProfNow: !prevState.isProf
-		}))
-	}
+	// 	this.setState(prevState => ({
+	// 		isHomeNow: !this.state.isHomeNow,
+	// 		isProfNow: !this.state.isProfNow
+	// 	}))
+	// }
 
 	render() {
 
@@ -42,15 +40,15 @@ class Footer extends Component {
 
 		return (
 			<div className = 'footer'>
-				<Link to={'/newsfeed'} onClick = {(e) => this.homeClick(e)}>
-       				<div className={`${isHome} ${isHomeNow}` }></div>
+				<Link to={'/newsfeed'} onClick = {(e) => this.handleClick(e)}>
+       				<div className={isHome}></div>
         		</Link>
       			<img className="button" src="https://res.cloudinary.com/drsaojfyp/image/upload/v1541516078/Screen_Shot_2018-11-06_at_9.48.07_AM.png" alt="Logo" />
       			<Link to = {`/AddPhoto/${username}`}><img className="button addPhoto" src="https://res.cloudinary.com/drsaojfyp/image/upload/v1541515748/Screen_Shot_2018-11-06_at_9.47.42_AM.png" alt="Camera"/>
       			</Link>
       			<img className="button notification" src="https://res.cloudinary.com/drsaojfyp/image/upload/v1541516172/Screen_Shot_2018-11-06_at_9.53.27_AM.png" alt="Camera"/>
       			<Link to={`/profilepage/${username}`} onClick = {(e) => this.handleClick(e)}>
-       				<div className={`${isProf}`}></div>
+       				<div className={isProf}></div>
         		</Link>
 			</div>
 		)
